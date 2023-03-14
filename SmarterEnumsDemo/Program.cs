58020633d2f824ecbe88c32d90bf632cc82e07df
Robert Lysebo <robertlysebo@gmail.com>
2023-03-14 12:10:04 +0100

    Self study material

---
+++ SmarterEnumsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using SmarterEnumsDemo;
+
+var free = Subscriptions.Free;
+var premium = Subscriptions.Premium;
+var vip = Subscriptions.Vip;
+
+var freeToo = Subscriptions.Free;
+
+var freeFromName = Subscriptions.FromName("Free");
+var freeFromValue = Subscriptions.FromValue(1);
+
+Console.WriteLine(freeFromValue == freeFromName);
+
+Console.WriteLine($"Discount was {freeFromName.Discount}");
